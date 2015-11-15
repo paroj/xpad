4ff66e1ed1af8cc3513529b39c0acc0d83276dac
Clement Calmels <clement.calmels@free.fr>
2015-11-15 15:38:04 +0100

    Input: xpad - remove spurious events of wireless xpad 360 controller
    When powering up a wireless xbox 360 controller, some wrong joystick
    events are generated. It is annoying because, for example, it makes
    unwanted moves in Steam big picture mode's menu.
    
    When my controller connects, this packet is received by the driver:
    00000000: 00 0f 00 f0 00 cc ff cf 8b e0 86 6a 68 f0 00 20
    ...........jh..
    00000010: 13 e3 20 1d 30 03 40 01 50 01 ff ff              .. .0.@.P...
    According to xboxdrv:
    https://github.com/xboxdrv/xboxdrv/blob/stable/src/xbox360_wireless_controller.cpp#L118
    this packet is only dumping a serial id and should not be interpreted
    as joystick events.
    
    This issue can be easily seen with: $ jstest --event /dev/input/js0
    This patch only adds a way to filter out this "serial" packet and as a
    result it removes the spurous events.
    
    Signed-off-by: Clement Calmels <clement.calmels@free.fr>

--- xpad.c
+++ xpad.c
@@ -554,7 +554,7 @@ static void xpad360w_process_packet(struct usb_xpad *xpad, u16 cmd, unsigned cha
 	}
 
 	/* Valid pad data */
-	if (!(data[1] & 0x1))
+	if (!(data[1] & 0x1) || data[1] == 0xf)
 		return;
 
 	xpad360_process_packet(xpad, cmd, &data[4]);
