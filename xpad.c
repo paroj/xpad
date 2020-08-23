e88f90d4e6d0d3a7bd336c40016119b73e1e45ef
Renaud Lepage <cybik@users.noreply.github.com>
2020-08-23 10:12:50 -0700

    Hyperkin Duke Variant 1.01
    A Hyperkin Duke with `idVendor=2e24` and `idProduct=1618` is known to be in the wild. Adding this DeviceID set in `/sys/bus/usb/drivers/xpad/new_id` allows xpad to operate the controller just fine, so this PR should be all xpad needs to operate the pad.

--- xpad.c
+++ xpad.c
@@ -144,6 +144,7 @@ static const struct xpad_device {
 	{ 0x045e, 0x02e3, "Microsoft X-Box One Elite pad", 0, XTYPE_XBOXONE },
 	{ 0x045e, 0x02ea, "Microsoft X-Box One S pad", 0, XTYPE_XBOXONE },
 	{ 0x2e24, 0x0652, "Hyperkin Duke X-Box One pad", 0, XTYPE_XBOXONE },
+	{ 0x2e24, 0x1618, "Hyperkin Duke X-Box One pad v1.01", 0, XTYPE_XBOXONE },
 	{ 0x045e, 0x0719, "Xbox 360 Wireless Receiver", MAP_DPAD_TO_BUTTONS, XTYPE_XBOX360W },
 	{ 0x046d, 0xc21d, "Logitech Gamepad F310", 0, XTYPE_XBOX360 },
 	{ 0x046d, 0xc21e, "Logitech Gamepad F510", 0, XTYPE_XBOX360 },
