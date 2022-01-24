0cd1afc2040fdbd777b46fa214ccb78686f31caa
Jasper Poppe <jgpoppe@gmail.com>
2022-01-24 17:15:28 +0100

    Add support for: Nacon - Daija Arcade Stick (PC/X-input mode)
    (Nacon USB vendor == BigBen)
    
    This stick requires: MAP_TRIGGERS_TO_BUTTONS to work properly, this patch takes care of that.

--- xpad.c
+++ xpad.c
@@ -261,6 +261,7 @@ static const struct xpad_device {
 	{ 0x1430, 0x8888, "TX6500+ Dance Pad (first generation)", MAP_DPAD_TO_BUTTONS, XTYPE_XBOX },
 	{ 0x1430, 0xf801, "RedOctane Controller", 0, XTYPE_XBOX360 },
 	{ 0x146b, 0x0601, "BigBen Interactive XBOX 360 Controller", 0, XTYPE_XBOX360 },
+	{ 0x146b, 0x0604, "Bigben Interactive DAIJA Arcade Stick", MAP_TRIGGERS_TO_BUTTONS, XTYPE_XBOX360 },
 	{ 0x1532, 0x0037, "Razer Sabertooth", 0, XTYPE_XBOX360 },
 	{ 0x1532, 0x0a00, "Razer Atrox Arcade Stick", MAP_TRIGGERS_TO_BUTTONS, XTYPE_XBOXONE },
 	{ 0x1532, 0x0a03, "Razer Wildcat", 0, XTYPE_XBOXONE },
