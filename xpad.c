6460408f180dfa4e5cf3b6f88899aa3e3f2a03a6
CMC Games <cleberdemattoscasali-1@yahoo.com.br>
2022-04-22 13:43:26 -0300

    Add support for Dareu H101 controller
    Add support for Dareu H101 controller (ID 260d:3002)
    https://en.dareu.com/product6/309.html
    
    Signed-off-by: Cleber de Mattos Casali <clebercasali@gmail.com>

--- xpad.c
+++ xpad.c
@@ -478,6 +478,7 @@ static const struct usb_device_id xpad_table[] = {
 	XPAD_XBOX360_VENDOR(0x24c6),		/* PowerA Controllers */
 	XPAD_XBOXONE_VENDOR(0x24c6),		/* PowerA Controllers */
 	XPAD_XBOX360_VENDOR(0x2563),		/* OneXPlayer Gamepad */
+	XPAD_XBOX360_VENDOR(0x260d),		/* Dareu H101 */
 	XPAD_XBOXONE_VENDOR(0x2dc8),		/* 8BitDo Pro 2 Wired Controller for Xbox */
 	XPAD_XBOXONE_VENDOR(0x2e24),		/* Hyperkin Duke X-Box One pad */
 	XPAD_XBOX360_VENDOR(0x2f24),		/* GameSir Controllers */
