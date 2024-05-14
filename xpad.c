1354136e468ef6c4a51a364a198f1e3cab12bddf
artewar67 <artewar67@yandex.ru>
2024-05-14 07:40:52 +0500

    Add support for Machenike G5 Pro controller

--- xpad.c
+++ xpad.c
@@ -389,6 +389,7 @@ static const struct xpad_device {
 	{ 0x3285, 0x0614, "Nacon Pro Compact", 0, XTYPE_XBOXONE },
 	{ 0x3767, 0x0101, "Fanatec Speedster 3 Forceshock Wheel", 0, XTYPE_XBOX },
 	{ 0x413d, 0x2104, "Black Shark Green Ghost Gamepad", 0, XTYPE_XBOX360 },
+	{ 0x2345, 0xe00b, "Machenike G5 Pro controller", 0, XTYPE_XBOX360, QUIRK_360_START_PKT_1 },
 	{ 0xffff, 0xffff, "Chinese-made Xbox Controller", 0, XTYPE_XBOX },
 	{ 0x0000, 0x0000, "Generic X-Box pad", 0, XTYPE_UNKNOWN }
 };
@@ -531,6 +532,7 @@ static const struct usb_device_id xpad_table[] = {
 	XPAD_XBOX360_VENDOR(0x3285),		/* Nacon GC-100 */
 	XPAD_XBOXONE_VENDOR(0x3285),		/* Nacon XBOX Series S/X Controllers */
 	XPAD_XBOX360_VENDOR(0x413d),		/* Black Shark Green Ghost Controller */
+	XPAD_XBOX360_VENDOR(0x2345),		/* Machenike G5 Pro controller */
 	{ }
 };
 
